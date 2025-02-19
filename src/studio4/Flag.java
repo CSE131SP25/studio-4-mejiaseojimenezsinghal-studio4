f2e937cebd7a373aea379e560a8e12a2d3f77960
mysbi <mysbi@Sarah_Jimenez.dhcp.wustl.edu>
2025-02-19 17:11:11 -0600

    Studio 4 - done!

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,10 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(101, 150, 219);
+		StdDraw.filledRectangle(0.5,0.5,0.4,.2);
+		StdDraw.setPenColor(13,25,117);
+		StdDraw.filledCircle(.5,.5,.07);
+		StdDraw.filledEllipse(0, 0, 0, 0);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,40 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeight = in.nextDouble();
+		
+		if (shape.equals("rectangle")) {
+				if (filled == true) {
+					StdDraw.setPenColor(red, green, blue);
+					StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+				}
+				else {
+					StdDraw.rectangle(x, y, halfWidth, halfHeight);
+				}		
+		}
+		if (shape == "ellipse") {
+			if (filled = true) {
+				StdDraw.filledEllipse(x, y, x, x);
+			}
+			else {
+				StdDraw.ellipse(x, y, x, x);
+			}
+		}
+		//if (shape == "triangle") {
+			//if (filled = true)
+			//{		
+//			}
+	//	}
+		
+		
+		
 	}
 }
